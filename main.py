9b673e922cae7a1d9551fd7aeadd91583756ec38
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-02-17 18:29:45 -0600

    feat: update main.py

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 class Solution:
+    def reverseVowels_2(self, s: str) -> str:
+        # create a Set of Vowels
+        vowels = set("aeiouAEIOU")
+        # Convert vowels to a list
+        s = list(s)
+        # define two pointers to move from both ends of the list moving inward
+        pointer_init = 0
+        pointer_end = len(s) - 1
+        # while pointer_init is not reached pointer_end
+        while pointer_init < pointer_end:
+            # while pointer_init is not reached pointer_end and letter in pointer_init is not a vowel
+            while pointer_init < pointer_end and s[pointer_init] not in vowels:
+                # Move pointer to the next letter
+                pointer_init += 1
+            # while pointer_init is not reached pointer_end and letter in pointer_end is not a vowel
+            while pointer_init < pointer_end and s[pointer_end] not in vowels: 
+                # Move pointer to the previous letter
+                pointer_end -= 1
+            # At this point both are vowels so lets swap them
+            s[pointer_init], s[pointer_end] = s[pointer_end], s[pointer_init]
+            # move pointer_init and pointter_end
+            pointer_init += 1
+            pointer_end -= 1
+        # Return reversed vowels word
+        return "".join(s)
+
+
     def reverseVowels(self, s: str) -> str:
         # create a Set of Vowels
         vowels = set("aeiouAEIOU")
